09eeaee62d3abfaf62df7d8850634f4ffb5c7e7e
Everyday Baller <everydayballer.training@gmail.com>
2025-06-07 17:52:51 +0300

    Updated Select.tsx

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -63,7 +63,7 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
             {...props}
           >
             {placeholder && (
-              <option value="" disabled>
+              <option value="\" disabled>
                 {placeholder}
               </option>
             )}
